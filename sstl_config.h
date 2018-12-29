712b06a460e8a4b3292005694f49e1515630ee62
Jason <duan_jason@outlook.com>
2018-12-28 20:32:35 -0500

    add config

---
+++ sstl_config.h
@@ -0,0 +1,13 @@
+#ifndef SSTL_DEF_H
+#define SSTL_DEF_H
+
+#define __SSTL_TRY
+
+#ifdef __SSTL_USE_EXCEPTIONS
+    #undef __SSTL_USE_EXCEPTIONS
+#endif
+
+#define __SSTL_TEMPLATE_NULL template <>
+
+
+#endif // SSTL_DEF_H
